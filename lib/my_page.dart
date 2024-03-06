89061d84fb24bda44a1d8462cf9984c3463bf272
quanghoatrinh <quanghoa.trinh@gmail.com>
2024-03-06 14:20:51 +0700

    demo change screen by bottom tab

--- lib/my_page.dart
+++ lib/my_page.dart
@@ -1,20 +1,45 @@
 
 import 'package:flutter/material.dart';
+import 'package:t2207a_flutter/screen/home/home_screen.dart';
+import 'package:t2207a_flutter/screen/profile/profile_screen.dart';
+import 'package:t2207a_flutter/screen/search/search_screen.dart';
 
-class MyPage extends StatelessWidget{
-  const MyPage({super.key});
+class MyPage extends StatefulWidget{
+    const MyPage({Key? key}) : super(key: key);
+
+    @override
+    _MyPageState createState() => _MyPageState();
+}
+
+class _MyPageState extends State<MyPage>{
+  final List<Widget> _screens = [
+    const HomeScreen(),
+    const SearchScreen(),
+    const ProfileScreen()
+  ];
+  int _selectedIndex = 0;
+
+  _changeTab(int index){
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
       appBar: AppBar(
-        title: const Text("T2207A Demo App"),
+        title: const Text("T2207A Demo App",
+        style: TextStyle(color: Colors.white)
+        ),
+        backgroundColor: Colors.orange,
       ),
-      body:const Text(
-        "Hello world!",
-        style: TextStyle(color: Colors.pink)
-      ),
+      body: _screens[_selectedIndex],
       bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.orange,
+        unselectedItemColor: Colors.black45,
+        onTap: (index) => _changeTab(index),
         items: [
           BottomNavigationBarItem(icon: Icon(Icons.home_max_outlined),label: "Home"),
           BottomNavigationBarItem(icon: Icon(Icons.search_off_outlined),label: "Search"),
